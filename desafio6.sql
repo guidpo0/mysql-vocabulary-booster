7ab99202abad6c6a341080ff13e38ff8a2987b7b
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-27 18:06:49 -0300

    Requisito 6 feito

---
+++ desafio6.sql
@@ -0,0 +1,28 @@
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
+job_history_department.JOB_TITLE AS 'Cargo',
+job_history_department.START_DATE AS 'Data de início do cargo',
+job_history_department.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS employees
+INNER JOIN
+(
+SELECT history_department.EMPLOYEE_ID,
+jobs.JOB_TITLE,
+history_department.JOB_ID,
+history_department.START_DATE,
+history_department.DEPARTMENT_NAME
+FROM hr.jobs AS jobs
+INNER JOIN
+(
+SELECT history.EMPLOYEE_ID,
+history.JOB_ID,
+history.START_DATE,
+department.DEPARTMENT_NAME
+FROM hr.job_history AS history
+INNER JOIN hr.departments AS department
+ON history.DEPARTMENT_ID = department.DEPARTMENT_ID
+) AS history_department
+ON jobs.JOB_ID = history_department.JOB_ID
+) AS job_history_department
+ON job_history_department.EMPLOYEE_ID = employees.EMPLOYEE_ID
+AND job_history_department.JOB_ID = employees.JOB_ID
+ORDER BY `Nome completo` ASC, Cargo ASC;
