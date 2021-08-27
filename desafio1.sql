f09c8ed9e021f7b2b4fafa542332c1cc2d7b8845
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-27 16:10:42 -0300

    Requisito 1 feito

--- desafio1.sql
+++ desafio1.sql
@@ -3,4 +3,4 @@ IF(r.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r
 ON c.REGION_ID = r.REGION_ID
-ORDER BY 'País' ASC;
+ORDER BY País ASC;
