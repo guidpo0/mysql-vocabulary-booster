5d1fb72f92c6f15c41c7ea9d1ee0ff0258e2390c
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-28 17:20:50 -0300

    Requisito 15 feito

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
+BEGIN
+SELECT t.`Média Salarial`
+FROM
+(
+SELECT j.JOB_TITLE,
+ROUND(AVG(e.SALARY),2) AS 'Média Salarial'
+FROM jobs AS j
+INNER JOIN employees AS e
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = 'Programmer'
+GROUP BY j.JOB_TITLE
+) AS t;
+END $$
+DELIMITER ;
