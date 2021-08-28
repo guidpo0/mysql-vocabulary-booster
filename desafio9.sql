44867f81b154bb51a7cedd43840114bcbadb53b3
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-28 15:23:33 -0300

    Requisito 9 feito

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS 'Nome completo',
+COUNT(orders.EmployeeId) AS 'Total de pedidos'
+FROM w3schools.employees AS employees
+INNER JOIN w3schools.orders AS orders
+ON employees.EmployeeID = orders.EmployeeID
+GROUP BY employees.EmployeeID
+ORDER BY `Total de pedidos` ASC;
