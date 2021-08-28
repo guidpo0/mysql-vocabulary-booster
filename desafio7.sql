61c1f09442ac28970a5a79fffb632a5bfde8faae
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-27 21:14:05 -0300

    Requisito 7 corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
-SELECT 
-UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
-HIRE_DATE AS 'Data de início',
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+j.START_DATE AS 'Data de início',
 SALARY AS 'Salário'
-FROM hr.employees
-WHERE MONTH(HIRE_DATE) IN (1, 2, 3)
-ORDER BY `Nome completo` ASC, HIRE_DATE ASC;
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS j
+ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
+WHERE MONTH(j.START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
